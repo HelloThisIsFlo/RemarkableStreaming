6e9b78aa2dbb7df7f445b065d094ed044e7e2145
Flo Kempenich <flo@kempenich.dev>
2023-06-24 12:09:25 +0100

    Add more sleep screens

--- sleep_suspended_screens/install_suspended_screen.sh
+++ sleep_suspended_screens/install_suspended_screen.sh
@@ -3,7 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
 
-scp $DIR/sleeping.png remarkable:/usr/share/remarkable
-scp $DIR/suspended.png remarkable:/usr/share/remarkable
+scp "$1" remarkable:/usr/share/remarkable/suspended.png
 
 cd - >/dev/null
